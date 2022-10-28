581459b38ec5ac4415a4238609f9add1dc9b14ee
Adam Parton <aparton1@stu.jsu.edu>
2022-10-28 11:18:29 -0500

    Added department class

---
+++ src/Department.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * @author Adam Parton
+ */
+public class Department {
+    
+    private int id;
+    private int terminalid;
+    private String description;
+
+    public Department(int id, int terminalid, String description) {
+        this.id = id;
+        this.terminalid = terminalid;
+        this.description = description;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public int getTerminalid() {
+        return terminalid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "ID: " + id + description + ", Terminal ID: " + terminalid + '}';
+    }
+    
+}
