7b231782942e4657884008d8b2ab13d03c5e3abc
Ryan Benton <rbenton1@stu.jsu.edu>
2022-10-31 11:31:14 -0500

    Fixed PunchDAO and Added Ahmeds Changes

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
@@ -1,60 +1,124 @@
 package edu.jsu.mcis.cs310.tas_fa22.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import edu.jsu.mcis.cs310.tas_fa22.Badge;
+import edu.jsu.mcis.cs310.tas_fa22.Department;
+import edu.jsu.mcis.cs310.tas_fa22.Punch;
+
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class PunchDAO {
 
-  private Connection connection;
+    private static final String QUERY_ID = "SELECT * FROM event WHERE id = ?;";
+    private static final String SQL_INSERT = "INSERT INTO event (terminalid, badgeid, timestamp, eventtypeid) VALUES (?, ?, ?, ?)";
+    private HashMap<String, String> map = new HashMap<>();
+    private DAOFactory daofactory = null;
+    private final BadgeDAO badgeDAO;
 
-  public PunchDAO() {
-    Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.geT= root;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} get(SQLException ) {
-				e.printStackTrace();
+    public PunchDAO(DAOFactory daoFactory) {
+        this.daofactory = daoFactory;
+        this.badgeDAO = new BadgeDAO(daofactory);
     }
-  }
 
-  Punch punch = new Punch();
-punch.setId(id);
-punch.setTerminalid(terminalid);
+    public Punch find(int id) {
+        Punch p = null;
+        PreparedStatement PS = null;
+        ResultSet RS = null;
 
-punches.add(punch);
-}
-} catch (SQLException e) {
-e.printStackTrace();
-}
+        try {
+            Connection conn = daofactory.getConnection();
 
-return punches;
-}
-        Badge badge = new Badge(resultSet.getString("badgeid"));
-        LocalDateTime originaltimestamp = resultSet.getTimestamp("originaltimestamp").toLocalDateTime();
-        EventType punchtype = Enum.valueOf(EventType.class, resultSet.getString("punchtype"));
-        punches.add(new Punch(id, terminalid, badge, originaltimestamp, punchtype));
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
+            if (conn.isValid(0)) {
+                PS = conn.prepareStatement(QUERY_ID);
+                PS.setInt(1, id);
+
+                boolean hasresults = PS.execute();
+
+                if (hasresults) {
+                    RS = PS.getResultSet();
+
+                    while (RS.next()) {
+
+                        map.put("id", RS.getString("id"));
+                        map.put("terminalid", RS.getString("terminalid"));
+                        map.put("badgeid", RS.getString("badgeid"));
+                        map.put("timestamp", RS.getString("timestamp"));
+                        map.put("eventtypeid", RS.getString("eventtypeid"));
+                        Badge badge = badgeDAO.find(RS.getString("badgeid"));
+                        p = new Punch(map, badge); // shift class using HashMap constructor
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        } finally {
+            if (RS != null) {
+                try {
+                    RS.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (PS != null) {
+                try {
+                    PS.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+        }
+        return p;
     }
-    return punches;
-  }
 
-  public void addPunch(Punch punch) {
-    try {
-      PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO punch VALUES (NULL, ?, ?, ?, ?)");
-      preparedStatement.setInt(1, punch.getTerminalid());
-      preparedStatement.setString(2, punch.getBadge().getId());
-      preparedStatement.setTimestamp(3, Timestamp.valueOf(punch.getOriginaltimestamp()));
-      preparedStatement.setString(4, punch.getPunchtype().name());
-      executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
+    public ArrayList<Punch> list(Badge b, LocalDate toLocalDate) {
+        return null;
     }
-  }
-}
 
+    public int create(Punch p1) {
+        PreparedStatement pst = null;
+
+        try {
+            Connection conn = daofactory.getConnection();
+            pst = conn.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+            Badge badge = p1.getBadge();
+            Department d = daofactory.getDepartmentDAO().find(badge);
+
+            if (p1.getTerminalid() != d.getTerminalid() && p1.getTerminalid() != 0) {
+                return 0;
+            }
+
+            pst.setInt(1, p1.getTerminalid());
+            pst.setString(2, p1.getBadgeid());
+            pst.setString(3, p1.getOriginaltimestamp().toString());
+            pst.setInt(4, p1.getEventtypeid());
+
+
+            int rows = pst.executeUpdate();
+
+            if (rows == 0) {
+                throw new DAOException("Creating user failed, no rows affected.");
+            }
+
+            ResultSet generatedKeys = pst.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                return generatedKeys.getInt(1);
+            }
+
+        } catch (Exception e) {
+            throw new DAOException(e.getMessage());
+
+        } finally {
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+        }
+        return 0;
+    }
+}
