a1eb350288149fa53c35759e10472bdeb06d8829
Ryan Benton <rbenton1@stu.jsu.edu>
2022-11-11 12:04:14 -0600

    Added Absenteeism Base

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOFactory.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOFactory.java
@@ -47,4 +47,8 @@ public final class DAOFactory {
     public DepartmentDAO getDepartmentDAO() {
         return new DepartmentDAO(this);
     }
+    
+    public AbsenteeismDAO getAbsenteeismDAO() {
+        return new AbsenteeismDAO(this);
+    }
 }
